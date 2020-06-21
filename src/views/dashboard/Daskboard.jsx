c89ef0483925c3d535caa4ac2ce30bada7a19bf5
trangtrangbk <tranginspirit78@gmail.com>
2020-06-21 18:56:47 +0700

    Chart on dashboard

--- src/views/dashboard/Daskboard.jsx
+++ src/views/dashboard/Daskboard.jsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core";
 import PeopleIcon from "@material-ui/icons/PeopleOutlined";
 import { Card, CardContent, Grid, Typography, Avatar } from "@material-ui/core";
-
-import { TotalUsers, TasksProgress, TotalProfit } from "./components";
-
+import request from "../../request";
+import DataChart from "react-apexcharts";
 const useStyles = makeStyles((theme) => ({
   root: {
     paddingTop: theme.spacing(12),
@@ -42,6 +41,51 @@ const useStyles = makeStyles((theme) => ({
 const Dashboard = () => {
   const classes = useStyles();
 
+  const [totalData, setTotalData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [year, setYear] = useState(new Date().getFullYear())
+  console.log()
+  const option = {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: [
+        "T1",
+        "T2",
+        "T3",
+        "T4",
+        "T5",
+        "T6",
+        "T7",
+        "T8",
+        "T9",
+        "T10",
+        "T11",
+        "T12",
+      ],
+    },
+    stroke: {
+      curve: "smooth",
+    },
+  };
+  const series = [
+    {
+      name: "series-1",
+      data: chartData.length>0?chartData.filter(chart =>chart.year== year)[0].data:[0,0,0,0,0,0,0,0,0,0,0,0] ,
+    },
+  ];
+
+  useEffect(() => {
+    request()
+      .get("/admin/statistic/counttotal")
+      .then((res) => setTotalData(res.data));
+    request()
+      .get("/admin/statistic/countNewsByMonth")
+      .then((res) => setChartData(res.data));
+  }, []);
+
+  console.log(totalData, chartData);
   return (
     <div className={classes.root}>
       <Grid container spacing={4}>
@@ -58,7 +102,7 @@ const Dashboard = () => {
                   >
                     TOTAL NEWS
                   </Typography>
-                  <Typography variant="h3">1,600</Typography>
+                  <Typography variant="h3">{totalData.totalNews}</Typography>
                 </Grid>
                 <Grid item>
                   <Avatar className={classes.avatar}>
@@ -82,7 +126,7 @@ const Dashboard = () => {
                   >
                     TOTAL USER
                   </Typography>
-                  <Typography variant="h3">1,600</Typography>
+                  <Typography variant="h3">{totalData.totalUser}</Typography>
                 </Grid>
                 <Grid item>
                   <Avatar className={classes.avatar}>
@@ -106,7 +150,7 @@ const Dashboard = () => {
                   >
                     TOTAL ADMIN
                   </Typography>
-                  <Typography variant="h3">1,600</Typography>
+                  <Typography variant="h3">{totalData.totalAdmin}</Typography>
                 </Grid>
                 <Grid item>
                   <Avatar className={classes.avatar}>
@@ -118,6 +162,8 @@ const Dashboard = () => {
           </Card>
         </Grid>
       </Grid>
+
+      <DataChart options={option} series={series} type="area" width="70%" />
     </div>
   );
 };
--- src/views/dashboard/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as TasksProgress } from './TasksProgress';
-export { default as TotalProfit } from './TotalProfit';
-export { default as TotalUsers } from './TotalUsers';
