fe9db659290d4c984a57c457ba80c97dac738a42
trangtrangbk <tranginspirit78@gmail.com>
2020-06-27 16:30:18 +0700

    Edit profile

--- src/views/profile/components/AccountDetails/AccountDetails.js
+++ src/views/profile/components/AccountDetails/AccountDetails.js
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from 'react';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles, InputLabel } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core";
 import request from "../../../../request";
 import {
   Card,
@@ -12,189 +12,239 @@ import {
   Grid,
   Button,
   TextField,
-  Select,
   MenuItem,
-  CircularProgress
-} from '@material-ui/core';
-import PermContactCalendarIcon from "@material-ui/icons/PermContactCalendar";
+  CircularProgress,
+} from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
-  root: {}
+  root: {},
 }));
 
-const AccountDetails = (props)=> {
+const AccountDetails = (props) => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
 
   const [address, setAddress] = useState("");
-  const [name, setName]=useState("");
-  const[gender,setGender]=useState("");
-  const[phoneNumber, setPhoneNumber]=useState("");
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [oldpass, setOldPass] = useState("");
+  const [newpass, setNewPass] = useState("");
   const [isSending, setIsSending] = useState(true);
-
+  const [messageSuccess, setMessageSuccess] = useState("");
+  const [messageError, setMessageError] = useState("");
   const [validate, setValidate] = useState({
     phone: false,
-  })
-  const ChangeInfor = (e)=>{
-//   // coi cai addamin
-        e.preventDefault();
-        setIsSending(true)
-        request().put("/userinfo",{
+  });
+  const ChangeInfor = (e) => {
+    e.preventDefault();
+    if (address == "" || name == "" || gender == "" || phoneNumber == "") {
+      setMessageError("Need to enter enough information!");
+      setMessageSuccess("");
+    } else {
+      setIsSending(true);
+      request()
+        .put("/userinfo", {
           address,
           name,
           gender,
-          phoneNumber
-        }).then(res =>{
-          setIsSending(false)
+          phoneNumber,
         })
-        .catch(err =>console.log(err))
- }
+        .then((res) => {
+          setIsSending(false);
+          setMessageSuccess("Update Information successfully");
+          setMessageError("");
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsSending(false);
+        });
+    }
+  };
 
-  useEffect(()=>{
-  // get use inforx
-    const id= JSON.parse(localStorage.getItem('user')).id;
-    request().get(`/userinfo/${id}`).then(res=>{
-    console.log(res.data)
-    const data = res.data
-    setAddress(data.address)
-    setGender(data.gender)
-    setPhoneNumber(data.phoneNumber)
-    setIsSending(false)
-   })
-   request().get(`/accounts/getAccount/${id}`).then(res=>{
-     setName(res.data.name)
-     setIsSending(false)
-   })
-},[])
+  const ChangePassword = () => {
+    if (newpass !== "") {
+      setIsSending(true);
+      request()
+        .patch("/accounts/changePassword", {
+          oldpass,
+          newpass,
+        })
+        .then((res) => {
+          setIsSending(false);
+          console.log("then");
+          setMessageSuccess("Password changed successfully");
+          setMessageError("");
+        })
+        .catch((err) => {
+          console.log(err);
+          setIsSending(false);
+          console.log("catch");
+          setMessageError("Old Password not correct!");
+          setMessageSuccess("");
+        });
+    }
+  };
+  console.log(messageError, messageSuccess);
+  useEffect(() => {
+    // get use infor
+    const id = JSON.parse(localStorage.getItem("user")).id;
+    request()
+      .get(`/userinfo/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        const data = res.data;
+        setAddress(data.address);
+        setGender(data.gender);
+        setPhoneNumber(data.phoneNumber);
+        setIsSending(false);
+      });
+    request()
+      .get(`/accounts/getAccount/${id}`)
+      .then((res) => {
+        setName(res.data.name);
+        setIsSending(false);
+      });
+  }, []);
 
   return (
     <div>
-{!isSending ?<Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-
-      <form
-        autoComplete="off"
-        noValidate
-      >
-        <CardHeader
-          subheader="The information can be edited"
-          title="Profile"
-        />
-        <Divider />
-        <CardContent>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-
-              <TextField
-                fullWidth
-                label="Name"
-                margin="dense"
-                name="Name"
-                onChange={(e)=>setName(e.target.value)}
-                required
-                value={name}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                select
-                label="Gender"
-                margin="dense"
-                name="gender"
-                onChange={(e)=>setGender(e.target.value)}
-                required
-                value={gender}
-                variant="outlined"
+      {!isSending ? (
+        <Card {...rest} className={clsx(classes.root, className)}>
+          <form autoComplete="off" noValidate>
+            <CardHeader
+              subheader="The information can be edited"
+              title="Profile"
+            />
+            <p style={{ color: "green" }}>{messageSuccess}</p>
+            <Divider />
+            <CardContent>
+              <Grid container spacing={3}>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Name"
+                    margin="dense"
+                    name="Name"
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    value={name}
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    select
+                    label="Gender"
+                    margin="dense"
+                    name="gender"
+                    onChange={(e) => setGender(e.target.value)}
+                    required
+                    value={gender}
+                    variant="outlined"
+                  >
+                    <MenuItem value={"true"}>Male</MenuItem>
+                    <MenuItem value={"false"}>Female</MenuItem>
+                  </TextField>
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Phone Number"
+                    margin="dense"
+                    name="phone"
+                    onChange={(e) => {
+                      setPhoneNumber(e.target.value);
+                      if (
+                        !isNaN(e.target.value) &&
+                        e.target.value.length == 10
+                      ) {
+                        let v = validate;
+                        v.phone = false;
+                        setValidate({ ...v });
+                      } else {
+                        let v = validate;
+                        v.phone = true;
+                        setValidate({ ...v });
+                      }
+                    }}
+                    required
+                    type="number"
+                    value={phoneNumber}
+                    variant="outlined"
+                    error={validate.phone}
+                    helperText={validate.phone ? "Invalid format" : ""}
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="Address"
+                    margin="dense"
+                    name="Address"
+                    onChange={(e) => setAddress(e.target.value)}
+                    required
+                    value={address}
+                    variant="outlined"
+                  ></TextField>
+                </Grid>
+                <Divider />
+                <CardActions style={{ width: "100%", display : "flex", justifyContent : "flex-end" }}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={() => ChangeInfor()}
+                  >
+                    Save profile
+                  </Button>
+                </CardActions>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="OldPassword"
+                    margin="dense"
+                    name="oldpass"
+                    onChange={(e) => setOldPass(e.target.value)}
+                    required
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label="NewPassword"
+                    margin="dense"
+                    name="newpass"
+                    onChange={(e) => setNewPass(e.target.value)}
+                    required
+                    variant="outlined"
+                  />
+                </Grid>
+                <p style={{ color: "red" }}>{messageError}</p>
+              </Grid>
+            </CardContent>
+            <CardActions style={{ width: "100%", display : "flex", justifyContent : "flex-end" }}>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={() => ChangePassword()}
               >
-                <MenuItem value={"true"}>Male</MenuItem>
-                <MenuItem value={"false"}>Female</MenuItem>
-              </TextField>
-           </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Phone Number"
-                margin="dense"
-                name="phone"
-                onChange={(e)=>{
-                  setPhoneNumber(e.target.value);
-                  if(!isNaN(e.target.value) && e.target.value.length == 10){
-                    let v = validate;
-                    v.phone = false;
-                    setValidate({...v});
-                  }else{
-                    let v = validate;
-                    v.phone = true;
-                    setValidate({...v});
-                  }
-                }}
-                required
-                type="number"
-                value={phoneNumber}
-                variant="outlined"
-                error={validate.phone}
-                helperText={validate.phone?"Invalid format":""}
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Address"
-                margin="dense"
-                name="Address"
-                onChange={(e)=>setAddress(e.target.value)}
-                required
-                value={address}
-                variant="outlined"
-              >
-              </TextField>
-            </Grid>
-
-          </Grid>
-        </CardContent>
-        <Divider />
-        <CardActions>
-          <Button
-            color="primary"
-            variant="contained"
-            onClick={ChangeInfor}
-          >
-            Save details
-          </Button>
-        </CardActions>
-      </form>
-    </Card>:(<div style={{textAlign : "center"}}>
-    <CircularProgress />
-    </div>)}
+                Save password
+              </Button>
+            </CardActions>
+          </form>
+        </Card>
+      ) : (
+        <div style={{ textAlign: "center" }}>
+          <CircularProgress />
+        </div>
+      )}
     </div>
-
   );
 };
 AccountDetails.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
 };
-export default AccountDetails
+export default AccountDetails;
